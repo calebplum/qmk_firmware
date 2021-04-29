ad84ba1cb4f182b88b5b40d4e628f0f41814e8ee
Caleb Plum <caleb.plum@megaport.com>
2021-04-29 14:17:01 +1000

    Swap modtap ltcl/tab with lalt/del to be lctl/del and lalt/tab

--- keyboards/keyboardio/atreus/keymaps/calebplum/keymap.c
+++ keyboards/keyboardio/atreus/keymaps/calebplum/keymap.c
@@ -269,7 +269,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                                KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    ,
     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                                KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN ,
     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_GRV,               KC_QUOT,              KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH ,
-    KC_ESC, KC_TAB, KC_LGUI,  KC_LSFT, KC_BSPC,  MT(MOD_LCTL, KC_TAB), MT(MOD_LALT, KC_DEL), KC_SPC,  MO(_RS), KC_MINS, KC_BSLS, KC_ENT ),
+    KC_ESC, KC_TAB, KC_LGUI,  KC_LSFT, KC_BSPC,  MT(MOD_LCTL, KC_DEL), MT(MOD_LALT, KC_TAB), KC_SPC,  MO(_RS), KC_MINS, KC_BSLS, KC_ENT ),
 
   /*
    *  !       @     up     $    %            ||            home    7     8     9    pgup
@@ -281,7 +281,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_EXLM,     KC_AT,   KC_UP,   KC_DLR,  KC_PERC,                                             KC_HOME, KC_7,    KC_8,   KC_9, KC_PLUS,
     KC_LPRN,     KC_LEFT, KC_DOWN, KC_RGHT, KC_RPRN,                                             KC_END,  KC_4,    KC_5,   KC_6, KC_EQL,
     KC_LBRC,     KC_RBRC, KC_HASH, KC_LCBR, KC_RCBR, KC_CIRC,              KC_AMPR,              KC_ASTR, KC_1,    KC_2,   KC_3, KC_PGUP,
-    TD(TD_LSEL), KC_INS,  KC_LGUI, KC_LSFT, KC_DEL,  MT(MOD_LCTL, KC_TAB), MT(MOD_LALT, KC_DEL), KC_SPC,  KC_TRNS, KC_0, KC_DOT, KC_PGDN ),
+    TD(TD_LSEL), KC_INS,  KC_LGUI, KC_LSFT, KC_DEL,  MT(MOD_LCTL, KC_DEL), MT(MOD_LALT, KC_TAB), KC_SPC,  KC_TRNS, KC_0, KC_DOT, KC_PGDN ),
 
   /*
    *  insert    home      up       end   pgup           ||           up     F7    F8    F9      F10
@@ -293,7 +293,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP,                                             KC_UP,   KC_F7,   KC_F8,   KC_F9,   KC_F10  ,
     KC_DEL,  KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,                                             KC_DOWN, KC_F4,   KC_F5,   KC_F6,   KC_F11  ,
     KC_MPRV, KC_VOLU, KC_MNXT, KC_NO,   RESET,   _______,              _______,              KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F12  ,
-    TO(_QW), KC_VOLD, KC_LGUI, KC_LSFT, KC_BSPC, MT(MOD_LCTL, KC_TAB), MT(MOD_LALT, KC_DEL), KC_SPC,  TO(_QW), KC_PSCR, KC_SLCK, KC_MPLY ),
+    TO(_QW), KC_VOLD, KC_LGUI, KC_LSFT, KC_BSPC, MT(MOD_LCTL, KC_DEL), MT(MOD_LALT, KC_TAB), KC_SPC,  TO(_QW), KC_PSCR, KC_SLCK, KC_MPLY ),
 
   /*
    *  _   _  _  _  _     ||     _   _  _  _  _
@@ -305,5 +305,5 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     WCSM,    WCSL,       KC_NO,   KC_NO,      KC_NO,                                               WCMAX,  WCSNAPTL,   WCSNAPUP,   WCSNAPTR,    KC_NO,
     KC_NO,   WCMONLEFT,  KC_NO,   WCMONRIGHT, KC_NO,                                               KC_NO,  WCSNAPLEFT, WCSNAPOFF,  WCSNAPRIGHT, KC_NO,
     KC_NO,   KC_NO,      KC_NO,   KC_NO,      KC_NO,  KC_GRV,               _______,               KC_NO,  WCSNAPBL,   WCSNAPDOWN, WCSNAPBR,    KC_NO,
-    TO(_QW), KC_TAB,     KC_LGUI, KC_LSFT,    KC_DEL, MT(MOD_LCTL, KC_TAB), MT(MOD_LALT, KC_DEL),  KC_SPC, TO(_QW),    KC_0,       KC_DOT,      KC_EQL )
+    TO(_QW), KC_TAB,     KC_LGUI, KC_LSFT,    KC_DEL, MT(MOD_LCTL, KC_DEL), MT(MOD_LALT, KC_TAB),  KC_SPC, TO(_QW),    KC_0,       KC_DOT,      KC_EQL )
 };

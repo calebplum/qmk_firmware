148057531154a74bc20a798b539739406ce24b3e
Caleb Plum <caleb.klein0@gmail.com>
2021-05-28 19:11:59 +1000

    Update function key to hold/toggle layer shift rather than just hold it

--- keyboards/keyboardio/atreus/keymaps/calebplum/keymap.c
+++ keyboards/keyboardio/atreus/keymaps/calebplum/keymap.c
@@ -269,7 +269,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                                KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    ,
     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                                KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN ,
     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_GRV,               KC_QUOT,              KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH ,
-    KC_ESC, KC_TAB, KC_LGUI,  KC_LSFT, KC_BSPC,  MT(MOD_LCTL, KC_DEL), MT(MOD_LALT, KC_TAB), KC_SPC,  MO(_RS), KC_MINS, KC_BSLS, KC_ENT ),
+    KC_ESC, KC_TAB, KC_LGUI,  KC_LSFT, KC_BSPC,  MT(MOD_LCTL, KC_DEL), MT(MOD_LALT, KC_TAB), KC_SPC,  TT(_RS), KC_MINS, KC_BSLS, KC_ENT ),
 
   /*
    *  !       @     up     $    %            ||            home    7     8     9    pgup
@@ -278,9 +278,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * lower  insert super shift del  ctrl/del ||  alt/tab  space    fn    0     .    =
    */
   [_RS] = LAYOUT( /* [> RAISE <] */
-    KC_EXLM,     KC_AT,   KC_UP,   KC_DLR,  KC_PERC,                                             KC_HOME, KC_7,    KC_8,   KC_9, KC_PLUS,
-    KC_LPRN,     KC_LEFT, KC_DOWN, KC_RGHT, KC_RPRN,                                             KC_END,  KC_4,    KC_5,   KC_6, KC_EQL,
-    KC_LBRC,     KC_RBRC, KC_HASH, KC_LCBR, KC_RCBR, KC_CIRC,              KC_AMPR,              KC_ASTR, KC_1,    KC_2,   KC_3, KC_PGUP,
+    KC_EXLM,     KC_AT,    KC_UP,   KC_DLR,  KC_PERC,                                             KC_HOME, KC_7,    KC_8,   KC_9, KC_PLUS,
+    KC_LPRN,     KC_LEFT,  KC_DOWN, KC_RGHT, KC_RPRN,                                             KC_END,  KC_4,    KC_5,   KC_6, KC_EQL,
+    KC_LBRC,     KC_RBRC,  KC_HASH, KC_LCBR, KC_RCBR, KC_CIRC,              KC_AMPR,              KC_ASTR, KC_1,    KC_2,   KC_3, KC_PGUP,
     TD(TD_LSEL), TT(_WC),  KC_LGUI, KC_LSFT, KC_DEL,  MT(MOD_LCTL, KC_DEL), MT(MOD_LALT, KC_TAB), KC_SPC,  KC_TRNS, KC_0, KC_DOT, KC_PGDN ),
 
   /*
